c48732ee263297d4060b63ccf8da5cc7f84918fe
lyleyyy <111568799+lyleyyy@users.noreply.github.com>
2023-09-15 15:47:57 +1000

    Add files via upload

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,18 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicous Food, Delived To You</h2>
+      <p>
+        Choose your favortie meal fom our broad selectio of available meals and
+        enjoy a delicous luch or diner at home.
+      </p>
+      <p>
+        All our meals are cooked with high-quaity ingredinets. just-in-tim and
+        of cours bt expecirnced chef!
+      </p>
+    </section>
+  );
+};
+export default MealsSummary;
